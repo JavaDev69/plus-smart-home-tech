d8943d75fe286429902f9e8e5a86ae6349cf75d7
JavaDev69 <ITrue33@yandex.ru>
2026-06-14 21:09:51 +0300

    fix KafkaAvroProducer

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/KafkaAvroProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/KafkaAvroProducer.java
@@ -7,14 +7,18 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.VoidSerializer;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.collector.serialization.EventSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.serialization.EventSerializer;
 
+import java.time.Duration;
 import java.util.Properties;
 
+import static ru.yandex.practicum.collector.kafka.EventTopics.HUB_TOPIC_V1;
+import static ru.yandex.practicum.collector.kafka.EventTopics.SENSOR_TOPIC_V1;
+
 @Component
-public class KafkaAvroProducer {
+public class KafkaAvroProducer implements AutoCloseable {
     private Producer<String, Object> producer;
 
     @PostConstruct
@@ -27,12 +31,29 @@ public class KafkaAvroProducer {
     }
 
     public void send(SensorEventAvro event) {
-        ProducerRecord<String, Object> record = new ProducerRecord<>(EventTopics.SENSOR_TOPIC_V1, event);
+        ProducerRecord<String, Object> record =
+                new ProducerRecord<>(
+                        SENSOR_TOPIC_V1,
+                        null,
+                        event.getTimestamp().toEpochMilli(),
+                        null,
+                        event);
         producer.send(record);
     }
 
     public void send(HubEventAvro event) {
-        ProducerRecord<String, Object> record = new ProducerRecord<>(EventTopics.HUB_TOPIC_V1, event);
+        ProducerRecord<String, Object> record =
+                new ProducerRecord<>(
+                        HUB_TOPIC_V1,
+                        null,
+                        event.getTimestamp().toEpochMilli(),
+                        null, event);
         producer.send(record);
+    }
+
+    @Override
+    public void close() {
+        producer.flush();
+        producer.close(Duration.ofSeconds(10));
     }
 }
