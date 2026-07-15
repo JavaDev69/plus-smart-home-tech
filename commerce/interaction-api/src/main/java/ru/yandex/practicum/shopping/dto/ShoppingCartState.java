ce2b60f771103eab270619682c1a3776c5953f9b
JavaDev69 <ITrue33@yandex.ru>
2026-07-15 11:40:17 +0300

    fix conflict

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/shopping/dto/ShoppingCartState.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.shopping.dto;
+
+/**
+ * @author Andrew Vilkov
+ * @created 10.07.2026 - 18:48
+ * @project plus-smart-home-tech
+ */
+public enum ShoppingCartState {
+    ACTIVE,
+    DEACTIVATE
+}
