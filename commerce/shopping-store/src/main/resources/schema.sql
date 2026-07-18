7590ec4647560419fffae22a0771757344f98874
JavaDev69 <ITrue33@yandex.ru>
2026-07-18 15:00:56 +0300

    resolve issues

--- commerce/shopping-store/src/main/resources/schema.sql
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS products
 (
     id               UUID PRIMARY KEY,
     product_name     VARCHAR(255)   NOT NULL,
-    description      VARCHAR(255)   NOT NULL,
+    description      VARCHAR,
     image_src        VARCHAR(255),
-    quantity_state   VARCHAR(255)   NOT NULL,
-    product_state    VARCHAR(255)   NOT NULL,
-    product_category VARCHAR(255),
-    price            NUMERIC(19, 2) NOT NULL
+    quantity_state   VARCHAR(30)    NOT NULL,
+    product_state    VARCHAR(30)    NOT NULL,
+    product_category VARCHAR(30),
+    price            NUMERIC(10, 2) NOT NULL
 );
